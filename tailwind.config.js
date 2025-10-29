6750547d735f73d737ec1a77786b091e8d4886cc
Damir Salakhov <damirsalakhov@gmail.com>
2025-10-30 00:07:42 +0500

    Setup Tailwind CSS v3 + DaisyUI: add config, npm scripts, update Procfile.dev

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/assets/**/*.{css,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+}
+
