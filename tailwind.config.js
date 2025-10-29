4a7a044d1bb1ca9007a5e735e34016c7468c59ab
Damir Salakhov <damirsalakhov@gmail.com>
2025-10-30 00:09:20 +0500

    Fix DaisyUI plugin: use .default export

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    require('daisyui').default || require('daisyui'),
   ],
   daisyui: {
     themes: ["light", "dark"],
